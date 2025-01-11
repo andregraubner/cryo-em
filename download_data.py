dff9ad84d7c98648617538f0460e6d4083a346cd
Andre <andre.graubner@web.de>
2025-01-11 22:09:04 +0100

    Add download logic for pretraining data

--- download_data.py
+++ download_data.py
@@ -1,6 +1,17 @@
 from cryoet_data_portal import Client, Dataset
+from tqdm import tqdm
 
 client = Client()
 
-dataset = Dataset.get_by_id(client, 10441)
-dataset.download_everything("./data")
+dataset_ids = [
+    10108, # cryo-FIB INS-1E
+]
+for dataset_id in dataset_ids:
+    
+    dataset = Dataset.get_by_id(client, dataset_id)
+    for run in tqdm(dataset.runs):
+        fname = f"data/dump/{dataset.id}_{run.name}.mrc"
+        if not os.path.exists(fname):
+            run.tomograms[0].download_mrcfile(fname)
+        else:
+            print("Already exists: skipping", fname)
